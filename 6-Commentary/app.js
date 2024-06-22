a847e0f154905508dd133522e562635ffd63ff04
jatinn27 <jatin121dhiman@gmail.com>
2024-06-22 12:13:04 +0530

    implemented commentary

---
+++ 6-Commentary/app.js
@@ -0,0 +1,166 @@
+const commentary = [
+  {
+    title: "zero",
+    text: [
+      "<strong>No run</strong>, dropped short by the bowler - batter backs away on the cut and misses",
+      "<strong>No run</strong>, flighted delivery on off, batter goes back and cant get the drive back past the bowler",
+      "<strong>No run</strong>, pushed through on off, batter makes room and punches it to cover",
+      "<strong>No run</strong>, the leg-break, pushed through flatter and quicker - batter played inside the line and lets it go",
+      "<strong>No run</strong>, a wild swipe from the batter - was the wrong 'un and he was looking to slog across the line, didn't miss the off-stump by much",
+      "<strong>No run</strong>, full on off-stump, defended back towards the bowler",
+      "<strong>No run</strong>, back of a length on leg-stump, cramps the batter for room and rushes him as he looks to pull, goes towards mid-on after hitting him high on the bat, did well to control it",
+    ],
+  },
+  {
+    title: "single",
+    text: [
+      "<strong>1 run</strong>, length delivery on middle, tucked into the leg-side towards mid-wicket for one",
+      "<strong>1 run</strong>, length delivery on the pads, clipped away to deep backward square leg",
+      "<strong>1 run</strong>, full on off-stump, knocked towards cover and a fumble allows them the single",
+      "<strong>1 run</strong>, flatter delivery on off-stump, worked just wide of mid-on for a quick single",
+      "<strong>1 run</strong>, tossed up on off, worked down towards long-on for a single",
+      "<strong>1 run</strong>, tad too full from the bowler - whipped through mid-wicket for a single",
+      "<strong>1 run</strong>, gets away with one! Slower cutter banged in short, batter got into a terrible tangle looking to slap this away into the off-side, it got big on him and lobbed towards point off the splice, there wasn't anyone close enough",
+      "<strong>1 run</strong>, tossed up full on the stumps, batter gets forward to drive to long-off",
+      "<strong>1 run</strong>, short of length outside off, punched to sweeper cover"
+    ],
+  },
+  {
+    title: "double",
+    text: [
+      "<strong>2 runs</strong>, length delivery on off-stump, off cutter and it's tucked away towards long leg, they scamper back for two",
+      "<strong>2 runs</strong>, full and just turning down leg, batter just used the pace to glance it very fine, chase for the fielder at short fine-leg but he does well",
+      "<strong>2 runs</strong>, cut to the right of point. The man in the deep sprints to his left to cut it off",
+      "<strong>2 runs</strong>, slower length delivery outside off, beaten by the change of pace as batter heaves it away towards deep square leg, lands short of the two men running in and they pinch two",
+      "<strong>2 runs</strong>, off cutter on a length just outside off, gently tapped towards mid-wicket for a couple, good running",
+      "<strong>2 runs</strong>, length delivery outside off, slashed away through the gap at extra cover, no timing on it and they'll get a couple",
+      "<strong>2 runs</strong>, pitched up by the bowler on off, batter drives it through cover for a couple of runs",
+    ],
+  },
+  {
+    title: "three",
+    text: [
+      "<strong>3 runs</strong>, tossed up delivery on off, batter drives it through cover for three runs. Found the gap to perfection but the outfield wasn't quick enough",
+      "<strong>3 runs</strong>, short ball, outside off, batter chops the ball down past point and the long fence means they get a triple. Fielder dived and then almost rolled over. He released the ball when he thought he will touch the cushion. Good fielding in the end",
+    ],
+  },
+  {
+    title: "four",
+    text: [
+      "<strong>FOUR</strong>, short of length, too predictable. batter stands tall, waits for it and slaps it over the non-striker's head. No chance for mid-off to even get close to it",
+      "<strong>FOUR</strong>, short of length, batter rocks back to pummel it with a pull shot through midwicket. Struck hard and flat, hits the rope on the half-volley",
+      "<strong>FOUR</strong>, beautifully played, brings out the reverse sweep - was full and right in the slot, clears backward point and the ball races away to the ropes",
+      "<strong>FOUR</strong>, creamed, on a good length and well wide of off - batter leans into the drive, presents the full face of the bat and finds the gap wide of cover",
+      "<strong>FOUR</strong>, superb wristwork! Full on off-stump, batter leant in and swept it hard and right into the gap at cow corner, generated so much power with just his wrists and it raced away",
+      "<strong>FOUR</strong>, This was a loopy delivery on off and batter drills it through cover, right into the gap and manages to beat the fielder in the deep as well",
+      "<strong>FOUR</strong>, glorious shot! All class. Advances down the wicket and drills the length delivery through wide mid-off does the batter. Couldn't have timed or placed it better",
+      "<strong>FOUR, edged and four,</strong> short and wide of off, batter saw the width and went hard on the cut, went flying off the top-edge and beats third man to his left",
+      "<strong>FOUR, cheeky and four,</strong> pushed through outside off - batter waits on it, opens the face of the bat and then runs it past the keeper, beating short third to his left",
+      "<strong>FOUR, useful runs for batter ,</strong> banged in short and at the right-hander, he's early into the pull and gets a thick inside edge, the ball racing away to the long leg boundary",
+      "<strong>FOUR</strong> and that's nailed through the gap for a four. For a change, bowler looks to bowl it up and batter square drove it between point and cover for a four. That was angled across and was fuller as well",
+    ],
+  },
+  {
+    title: "six",
+    text: [
+      "<strong>SIX</strong>, <strong>such a clean strike!</strong> Just short of length on off-stump, batter hung back and watched it nicely before smashing it over the mid-wicket ropes with a jab, very fast hands",
+      "<strong>SIX</strong>, <strong>launched!!</strong> Pitched up and in the swinging arc, Pooran stays in his crease and it is just a case of range hitting then for the dangerous southpaw. A clean swing across the line over deep mid-wicket into the stands",
+      "<strong>SIX</strong>, <strong>jeez, where do you bowl?</strong> bowler tries to go wide outside off, tries to hide it from the swinging arc. batter gets closer to reach for it and slices it over extra cover for a maximum.",
+      "<strong>SIX</strong>, <strong>shot!</strong> Excellent pick-up shot. Length ball slanted in and that's been dumped into the square leg stands. Good swing of the bat and the ball flies all the way",
+      "<strong>SIX</strong>, <strong>batter on the charge!</strong> Full delivery at the stumps, right in the slot and batter launches it over wide long-on. Lovely clean swing of the bat as always",
+      "<strong>SIX</strong>, that's a smash me ball from bowler. Short leg-side boundary and it's a leg-side freebie to hit with the wind over backward square leg and batter pounces first ball",
+      "<strong>SIX</strong>, <strong>in the airrrr</strong> and just over deep backward square leg again. fielder jumps but it has enough legs. Third full toss from bowler as he misses the yorker. Wonder why they planed to go for the yorker and not look to use the pitch with pace off. batter is not complaining. Slogged away over the boundary",
+      "<strong>SIX</strong>, <strong>beautifully played,</strong> they're playing the conditions perfectly - was the leg-break from bowler, batter made most of his reach, leant forward and then played an open faced drive, hit into the wind and the ball kept sailing high, and over wide long-off",
+      "<strong>SIX</strong>, <strong>clubbed,</strong> the bottom-handed shovel from batter - dragged it from well wide of off, hit it flat and hard - kept long-on in the game, but he can only watch it soar over his head for a maximum",
+      "<strong>SIX</strong>, <strong>reverse sweep for six!!</strong> Pitched on middle and spun onto leg, batter times his reverse sweep perfectly and lifts it over backward point all the way for six",
+      "<strong>SIX</strong>, <strong>hammered,</strong> too full once again, allows batter to get under it on the slog-sweep, he's hit it into the wind and the ball sails over the ropes for a maximum",
+      "<strong>SIX</strong>, a misjudgement in the deep and <strong>bowler watches the ball sail over his head</strong> - was full and just outside off, sliced away with the wind over cover, bowler ran in and then has to go back, watches it sail over his head - would've been a half-chance had he stayed right back",
+      "<strong>SIX oh what a shot!</strong>, fuller than a length outside off, batter backs away, crouches low and carves it over cover for six. Wow that's a gem of a shot",
+      "<strong>SIX</strong>, tossed up on off, right in the slot for batter who swipes it in the air and clears wide long-on. Probably was the googly but it was too full",
+    ],
+  },
+];
+
+let count = 2;
+
+const value = document.querySelector("#total");
+const btnss = document.querySelectorAll(".cbtn");
+
+btnss.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("inc")) {
+      count++;
+    } else if (styles.contains("dec")) {
+      count--;
+      if (count < 1) {
+        count = 1;
+      }
+    }
+    value.textContent = `(${count})`;
+  });
+});
+
+// console.log(currOver.getAttribute('data-max'));
+
+const btns = document.querySelectorAll(".btn");
+const commentaryContainer = document.querySelector(".commentary");
+const getRandomNumber = (item) => {
+  return Math.floor(Math.random() * item.text.length);
+};
+
+const displayCommentary = (comm, over, balls) => {
+  let currComm = comm
+    .map((item) => {
+      const random = getRandomNumber(item);
+      return `<div class="over">
+      <div class="over-number">${over}.${balls}</div>
+      <div class="commentary-text">
+        ${item.text[random]}
+      </div>
+    </div>`;
+    })
+    .join("");
+
+  commentaryContainer.insertAdjacentHTML("afterbegin", currComm);
+};
+
+let over = 0;
+let balls = 0;
+const currOver = document.querySelector(".curr-over");
+btns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const key = btn.getAttribute("data-key");
+
+    const audio = document.querySelector(`audio[data-key="${key}"]`);
+
+    audio.currentTime = 0;
+    audio.play();
+
+    const title = e.currentTarget.dataset.id;
+    const corresComm = commentary.filter((comm) => {
+      if (comm.title == title) {
+        return comm;
+      }
+    });
+
+    currOver.setAttribute("data-max", `${count}`);
+    const maxOvers = currOver.getAttribute("data-max");
+    const error = document.querySelector(".error");
+
+    if (over < maxOvers) {
+      balls += 1;
+
+      displayCommentary(corresComm, over, balls);
+      if (balls === 6) {
+        over += 1;
+        balls = 0;
+      }
+      currOver.textContent = `${over}.${balls}`;
+      error.classList.add("hide-error");
+    } else {
+      error.classList.remove("hide-error");
+      error.innerHTML = `*** Max Overs reached ***`;
+    }
+  });
+});
