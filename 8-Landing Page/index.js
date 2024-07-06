46014ba694e33332908f6e739a6d0aee8a1ebbc6
jatinn27 <jatin121dhiman@gmail.com>
2024-07-06 22:26:40 +0530

    ..

--- 8-Landing Page/index.js
+++ 8-Landing Page/index.js
@@ -4,3 +4,32 @@ const options = document.querySelector('.options')
 navToggle.addEventListener('click', function () {
   options.classList.toggle("show-links");
 })
+
+const data = [...document.querySelectorAll('.data')]
+
+const updateCount = (item) => {
+  const value = parseInt(item.dataset.val);
+  const id = parseInt(item.dataset.id);
+
+  const increment = Math.ceil(value / 1000)
+  let initialValue = 0
+
+  const increaseCount = setInterval(() => {
+    initialValue += increment
+
+    if (initialValue > value) {
+      if(id == 1){
+        item.textContent = `${value}k+`
+      }else{
+        item.textContent = `${value}+`
+      }
+      clearInterval(increaseCount)
+      return
+    }
+    item.textContent = `${initialValue}+`
+  }, 10)
+}
+
+data.forEach((item) => {
+  updateCount(item)
+})
